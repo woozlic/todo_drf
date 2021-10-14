4892397a95c2c4cc6d5a40902993da649f538081
woozlic <sampaccs@mail.ru>
2021-10-14 10:57:34 +0500

    add swagger documentation and versioning

--- backend/todo_drf/urls.py
+++ backend/todo_drf/urls.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from user.views import UserViewSet
 from todo.views import TodoViewSet, ProjectViewSet
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Todos",
+        default_version="0.1",
+        description="Documentation to todos project",
+        contact=openapi.Contact(email="sampaccs@mail.ru"),
+        license=openapi.License(name="MIT License")
+    ),
+    public=True,
+    permission_classes=(AllowAny,)
+)
 
 
 router = DefaultRouter()
@@ -19,5 +36,7 @@ urlpatterns = [
     path('api-auth', include('rest_framework.urls')),
     path('api-token-auth/', views.obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui")
 ]
