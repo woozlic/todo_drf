5604a179941a131f8ecf1af67434c69dacc7d159
woozlic <sampaccs@mail.ru>
2021-10-19 13:56:30 +0500

    Revert "add mui to login page"
    This reverts commit 5eebf000dbee8fca273a244e5f6c5bfad98fdf2a.

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,9 +1,5 @@
 import React from "react";
 import {useState} from "react";
-import {CardHeader, Grid, TextField} from "@mui/material";
-import {Button} from "@mui/material"
-import {Card, CardContent} from "@mui/material";
-
 
 export default function LoginForm(props) {
     const [username, setUsername] = useState('')
@@ -23,39 +19,13 @@ export default function LoginForm(props) {
         event.preventDefault()
     }
     return (
-        <Card>
-            <CardContent>
-                <Grid component="form" onSubmit={(event => handleSubmit(event))}>
-                    <Grid container justifyContent="center">
-                        <CardContent>
-                            <TextField
-                                id="outlined-basic"
-                                label="Username"
-                                type="username"
-                                name="username"
-                                value={username}
-                                onChange={(event => handleChange(event))}
-                            />
-                        </CardContent>
-                    </Grid>
-                    <Grid container justifyContent="center">
-                        <CardContent>
-                            <TextField
-                                id="outlined-password-input"
-                                label="Password"
-                                type="password"
-                                name="password"
-                                autoComplete="current-password"
-                                value={password}
-                                onChange={(event => handleChange(event))}
-                            />
-                        </CardContent>
-                    </Grid>
-                    <Grid container justifyContent="center">
-                        <Button type="submit" variant="contained" style={{margin: "0 auto"}}>Login</Button>
-                    </Grid>
-                </Grid>
-            </CardContent>
-        </Card>
+        <div>
+            <div>Login page</div>
+            <form onSubmit={(event => handleSubmit(event))}>
+                <input type="text" name="username" placeholder="Username" value={username} onChange={(event => handleChange(event))} />
+                <input type="password" name="password" placeholder="Password" value={password} onChange={(event => handleChange(event))} />
+                <input type="submit" value="Login" />
+            </form>
+        </div>
     )
 }
